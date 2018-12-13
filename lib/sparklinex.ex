fae9706ea9017129b778d3bc329f0af484b1c2d4
Andrew Selder <aselder@usertesting.com>
2018-12-13 15:57:12 -0800

    New empty project.

---
+++ lib/sparklinex.ex
@@ -0,0 +1,18 @@
+defmodule Sparklinex do
+  @moduledoc """
+  Documentation for Sparklinex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sparklinex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sparklinex_test.exs
@@ -0,0 +1,8 @@
+defmodule SparklinexTest do
+  use ExUnit.Case
+  doctest Sparklinex
+
+  test "greets the world" do
+    assert Sparklinex.hello() == :world
+  end
+end
