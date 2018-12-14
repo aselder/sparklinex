5710144327ad6de42bb07622150ab5a21364ac03
Andrew Selder <aselder@usertesting.com>
2018-12-13 16:04:09 -0800

    Add some basic libraries.

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule Sparklinex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:mogrify, "~> 0.7.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false}
     ]
   end
 end
